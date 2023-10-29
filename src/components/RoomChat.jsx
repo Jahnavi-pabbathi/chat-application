4d103fe150b20539a3544aa7f2882c6e9cdc5133
Z00D0YK <Pabbathi.Jahnavi@target.com>
2023-10-29 23:02:38 +0530

    "succesfully sending messages to chat room"

--- src/components/RoomChat.jsx
+++ src/components/RoomChat.jsx
@@ -2,30 +2,29 @@ import React, { useContext, useState, useEffect } from "react"
 import { over } from "stompjs"
 import SockJS from "sockjs-client"
 import { useNavigate } from "react-router-dom";
+import { Grid } from "@mui/material";
+import Box from "@mui/material/Box";
+import './form.css'
 //import UserContext from "../UserContext";
 
 var stompClient = null;
 export const RoomChat = () => {
     // const [userName,setUserName] =useContext(UserContext)
     const userName = localStorage.getItem("userName");
-    const [publicChats, setPublicChats] = useState("");
+    const [publicChats, setPublicChats] = useState([]);
     const navigate = useNavigate();
-    const [userData, setUserData] = useState({
-        name: userName,
-        receiverName: "",
-        connected: false,
-        message: ""
-    })
+    const [messageToSend, setMessageToSend] = useState("");
+    const [connected, setConnected] = useState(false)
 
     const onConnected = () => {
-        setUserData({ ...userData, "connected": true })
+
+        setConnected(true)
         stompClient.subscribe('/chatroom/public', onPublicMessageReceived)
     }
 
     const onPublicMessageReceived = (payload) => {
         let payloadData = JSON.parse(payload.body);
-
-     //  setUserData({...userData,"name":payloadData.name,"message":payloadData.message})
+        setPublicChats((prevChats) => [...prevChats, payloadData]);
 
     }
 
@@ -40,9 +39,9 @@ export const RoomChat = () => {
     }
 
     const sendMessage = () => {
-        console.log(publicChats)
+        console.log(messageToSend)
         var message = {
-            content: publicChats,
+            content: messageToSend,
             messageType: 'CHATROOM',
             senderInfo: {
                 senderId: '1234',
@@ -53,46 +52,61 @@ export const RoomChat = () => {
                 receiverName: 'Bhargav'
             }
         };
-        try{
+        try {
             stompClient.send('/chat-service/chatroom/message', {}, JSON.stringify(message));
+            setMessageToSend("");
             console.log("sent successfully")
-            console.log("usr name ",userName)
+            console.log("usr name ", userName)
         }
-        catch(exception ){
+        catch (exception) {
             console.log(exception);
             console.log("sorry i failed")
         }
-        
-       
+
+
     }
 
 
     useEffect(() => {
         registerUser();
-    },[]);
+    }, []);
 
     return (
-        <div className="container">
-            {userData.connected ?
-                <>
+        <Grid container spacing={0} className="container">
+            <Grid item xs={3} style={{ backgroundColor: "#6c88c8" }} className="sidebar">
+
+            </Grid>
+            <Grid item xs={9} style={{ backgroundColor: "lightgray" }}>
+                {connected ? (
                     <div className="chat-box">
                         <input
                             type="text"
-                            id="messageInput"
-                            value={publicChats}
-                            onChange={(e) => setPublicChats(e.target.value)}
-                            placeholder="Type a message..." />
-                        <button onClick={sendMessage}>Send</button>
+                            className="input-field"
+                            value={messageToSend}
+                            onChange={(e) => setMessageToSend(e.target.value)}
+                            placeholder="Type a message..."
+                        />
+                        <button className="button" onClick={sendMessage}>
+                            Send
+                        </button>
                     </div>
-                    <br />
-                    <div>
-                        {userData.message}
-                    </div>
-                </>
+                ) : (
+                    navigate("/")
+                )}
+                <div className="chat-content">
+                    {publicChats.map((chat, index) => (
+                        <div key={index} className="message">
+                           
+                            
+                            <div className="content">
+                                <div className="userName">{userName}:</div>
+                                <div className="chatMessage">{chat.content}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </Grid>
+        </Grid>
 
-                : navigate("/")
-            }
-
-        </div>
     )
 }
